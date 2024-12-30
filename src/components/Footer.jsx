1d9aa93e75e7a488b82ca001c4b4a0938b36eab1
bastianorte <bastianort@gmail.com>
2024-12-30 00:34:28 -0300

    ajustes varios

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ export default function Footer() {
   return (
     <footer className="text-center bg-primary lg:text-left bottom-0">
       <div className="flex w-full max-w-48 flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between mx-auto">
-          <a href="#" className="text-lg font-medium text-white hover:underline"><FaInstagram /></a>
-          <a href="#" className="text-lg font-medium text-white hover:underline"><FaXTwitter /></a>
-          <a href="#" className="text-lg font-medium text-white hover:underline"><FaFacebookF /></a>
+          <a href="#" className="text-lg font-medium text-white hover:underline pb-2"><FaInstagram /></a>
+          <a href="#" className="text-lg font-medium text-white hover:underline pb-2"><FaXTwitter /></a>
+          <a href="#" className="text-lg font-medium text-white hover:underline pb-2"><FaFacebookF /></a>
       </div>
       <div className="p-4 text-center text-neutral-700 dark:text-neutral-200">
         © 2024 PuntoSalud Copyright
