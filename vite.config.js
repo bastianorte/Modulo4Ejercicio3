91d7e4cc5bf5c490d9cdbf1661063a37e80ee8c4
bastianorte <bastianort@gmail.com>
2025-04-02 02:19:16 -0300

    Ajustes dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // Establece el directorio de salida para los archivos generados por la construcción
+    outDir: 'build/client',  // Aquí defines el directorio donde se generarán los archivos finales
+    rollupOptions: {
+      input: {
+        main: 'index.html',  // Archivos de entrada (por lo general, el archivo HTML principal)
+      },
+    },
+  },
+});
